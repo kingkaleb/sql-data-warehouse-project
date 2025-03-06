8911a16c71a5265b1756d74c98ff7c224979f446
Kaleb Berhanu <bkaleb32@gmail.com>
2025-03-06 16:57:59 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,25 @@
+use master;
+GO
 
+--Drop and recreate the 'DataWarehouse' database
+IF EXISTS(SELECT 1 FROM sys.databases where name ='DataWarehouse')
+begin
+	alter DATABASE DataWarehouse set Single_user with rollback immediate;
+	drop database DataWarehouse;
+end;
+Go
+--create the 'DataWarehouse' database
+create database Datawarehouse;
+GO
+
+use DataWarehouse;
+GO
+
+create schema bronze;
+GO
+
+create schema silver;
+GO
+
+create schema gold;
+GO
